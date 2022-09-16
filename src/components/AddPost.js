d1c67af0b219024701e4d494991fe84f52ca2833
Silentius222 <79799875+Silentius222@users.noreply.github.com>
2022-09-16 17:07:21 +0200

    Add files via upload

---
+++ src/components/AddPost.js
@@ -0,0 +1,9 @@
+const AddPost = (props) => {
+    return( <form>
+        <textarea placeholder="Add post..."></textarea>
+        <button className="btn">Add</button>
+    </form>
+    );
+};
+
+export default AddPost;
---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,16 @@
+import {Route, Routes} from "react-router-dom";
+import Home from "../views/Home";
+import Login from "../views/Login";
+import SignUp from "../views/SignUp";
+
+const AppRoutes = (props) => {
+    return(
+        <Routes>
+            <Route path="/" element={<Home user={props.user}/>} />
+            <Route path="/Login" element={<Login user={props.user} setUser={props.setUser}/>} />
+            <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+    );
+}
+
+export default AppRoutes;
